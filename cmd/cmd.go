3529936b65cbea3b27462e2152ef3111c015c7ad
Zachary Huff <zach.huff.386@gmail.com>
2016-07-21 01:20:40 -0400

    Remove init hue

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -2,7 +2,6 @@ package cmd
 
 import (
 	"github.com/zachhuff386/hue-alert/config"
-	"github.com/zachhuff386/hue-alert/hue"
 	"github.com/zachhuff386/hue-alert/logger"
 )
 
@@ -17,28 +16,6 @@ func initConfig() (err error) {
 	err = config.Save()
 	if err != nil {
 		return
-	}
-
-	return
-}
-
-func initHue() (he hue.Hue, err error) {
-	he = hue.Hue{
-		Host:     config.Config.Host,
-		Username: config.Config.Username,
-	}
-
-	if config.Config.Username == "" {
-		err = he.Register()
-		if err != nil {
-			return
-		}
-
-		config.Config.Username = he.Username
-		err = config.Save()
-		if err != nil {
-			return
-		}
 	}
 
 	return
