d048f2de0f82b1f97aeb37b36c95527290b3ebf7
Zachary Huff <zach.huff.386@gmail.com>
2016-07-21 01:18:49 -0400

    Add account remove

--- cmd/accounts.go
+++ cmd/accounts.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"github.com/zachhuff386/hue-alert/account"
 	"github.com/zachhuff386/hue-alert/accounts"
+	"github.com/zachhuff386/hue-alert/config"
 )
 
 func Accounts() (err error) {
@@ -21,6 +22,44 @@ func Accounts() (err error) {
 
 	for _, acct := range accts {
 		fmt.Printf("%s: %s\n", acct.Type, acct.Identity)
+	}
+
+	return
+}
+
+func AccountRemove(typ, identity string) (err error) {
+	err = initConfig()
+	if err != nil {
+		return
+	}
+
+	account.InitAccounts()
+
+	accts, err := accounts.GetAccounts()
+	if err != nil {
+		return
+	}
+
+	removed := false
+
+	for _, acct := range accts {
+		if acct.Type == typ && acct.Identity == identity {
+			ok, e := config.Config.RemoveAccount(acct.Id)
+			if e != nil {
+				err = e
+				return
+			}
+
+			if ok {
+				removed = true
+			}
+		}
+	}
+
+	if removed {
+		fmt.Println("Account successfully removed")
+	} else {
+		fmt.Println("Account not found")
 	}
 
 	return
