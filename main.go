eb7485e1af80c1d27bc8343399446509730ff71c
Zachary Huff <zach.huff.386@gmail.com>
2016-07-21 00:59:03 -0400

    Add commands to main

--- main.go
+++ main.go
@@ -1,11 +1,24 @@
 package main
 
 import (
+	"flag"
 	"github.com/zachhuff386/hue-alert/cmd"
 )
 
 func main() {
-	err := cmd.Alert()
+	flag.Parse()
+
+	var err error
+
+	switch flag.Arg(0) {
+	case "google":
+		err = cmd.Google()
+	case "accounts":
+		err = cmd.Accounts()
+	case "remove":
+		err = cmd.Remove(flag.Arg(1), flag.Arg(2))
+	}
+
 	if err != nil {
 		panic(err)
 	}
