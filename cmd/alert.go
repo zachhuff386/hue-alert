bb7d6d2218ee2c753c0dd4cc44995bb4e10f1be0
Zachary Huff <zach.huff.386@gmail.com>
2016-07-21 02:06:58 -0400

    Add start command

--- cmd/alert.go
+++ cmd/alert.go
@@ -1,56 +1,42 @@
 package cmd
 
 import (
+	"github.com/zachhuff386/hue-alert/account"
+	"github.com/zachhuff386/hue-alert/alert"
 	"github.com/zachhuff386/hue-alert/config"
-	"github.com/zachhuff386/hue-alert/hue"
-	"github.com/zachhuff386/hue-alert/notification"
 	"os"
 	"os/signal"
 	"syscall"
 	"time"
 )
 
-func Alert() (err error) {
+func Start() (err error) {
 	err = initConfig()
 	if err != nil {
 		return
 	}
 
-	he := hue.Hue{
-		Host:     config.Config.Host,
-		Username: config.Config.Username,
-	}
-
-	lights, err := he.GetLights()
-	if err != nil {
+	he := initHue()
+	if he == nil {
 		return
 	}
 
-	notf := notification.Notification{
-		Transition: 500 * time.Millisecond,
-		Rate:       500 * time.Millisecond,
+	account.InitAccounts()
+
+	alrt := alert.Alert{
+		Hue:    he,
+		Lights: config.Config.Lights,
+		Rate:   5 * time.Second,
 	}
 
-	for _, light := range lights {
-		notf.AddLight(light)
-	}
-
-	alrt := notification.Alert{
-		Type:     "google",
-		Color:    "#dd4c40",
-		Duration: 500 * time.Millisecond,
-	}
-
-	notf.AddAlert(alrt)
-
-	notf.Run()
+	alrt.Run()
 
 	sig := make(chan os.Signal, 1)
 	signal.Notify(sig, os.Interrupt)
 	signal.Notify(sig, syscall.SIGTERM)
 	go func() {
 		<-sig
-		notf.Stop()
+		alrt.Stop()
 		os.Exit(1)
 	}()
 
