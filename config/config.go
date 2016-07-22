f6091d38cc12f1ddb2be9abda2f4fac87dc80f58
Zachary Huff <zach.huff.386@gmail.com>
2016-07-22 00:51:09 -0400

    Add brightness and mode to config

--- config/config.go
+++ config/config.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"github.com/dropbox/godropbox/errors"
 	"github.com/zachhuff386/hue-alert/account"
+	"github.com/zachhuff386/hue-alert/constants"
 	"github.com/zachhuff386/hue-alert/errortypes"
 	"io/ioutil"
 	"os"
@@ -14,17 +15,22 @@ const (
 	logPathDefault    = "hue.log"
 	serverPortDefault = 9300
 	serverHostDefault = "localhost"
+	brightnessDefault = 254
 )
 
 var Config = &ConfigData{}
 
 type ConfigData struct {
+	path       string   `json:"path"`
+	loaded     bool     `json:"-"`
 	Host       string   `json:"host"`
 	Username   string   `json:"username"`
 	Lights     []string `json:"lights"`
 	LogPath    string   `json:"log_path"`
 	ServerPort int      `json:"server_port"`
 	ServerHost string   `json:"server_host"`
+	Mode       string   `json:"mode"`
+	Brightness int      `json:"brightness"`
 	Google     struct {
 		Rate         int    `json:"rate"`
 		ClientId     string `json:"client_id"`
@@ -36,6 +42,101 @@ type ConfigData struct {
 		ClientSecret string `json:"client_secret"`
 	} `json:"slack"`
 	Accounts map[string]*account.Account `json:"accounts"`
+}
+
+func (c *ConfigData) Load(path string) (err error) {
+	c.path = path
+
+	_, err = os.Stat(c.path)
+	if err != nil {
+		if os.IsNotExist(err) {
+			err = nil
+		} else {
+			err = &errortypes.ReadError{
+				errors.Wrap(err, "config: File stat error"),
+			}
+		}
+		return
+	}
+
+	file, err := ioutil.ReadFile(c.path)
+	if err != nil {
+		err = &errortypes.ReadError{
+			errors.Wrap(err, "config: File read error"),
+		}
+		return
+	}
+
+	err = json.Unmarshal(file, Config)
+	if err != nil {
+		err = &errortypes.ReadError{
+			errors.Wrap(err, "config: File unmarshal error"),
+		}
+		return
+	}
+
+	if c.Lights == nil {
+		c.Lights = []string{}
+	}
+
+	if c.LogPath == "" {
+		c.LogPath = logPathDefault
+	}
+
+	if c.ServerPort == 0 {
+		c.ServerPort = serverPortDefault
+	}
+
+	if c.ServerHost == "" {
+		c.ServerHost = serverHostDefault
+	}
+
+	if c.Accounts == nil {
+		c.Accounts = map[string]*account.Account{}
+	}
+
+	if !constants.Modes.Contains(c.Mode) {
+		c.Mode = constants.Medium
+	}
+
+	if c.Brightness == 0 {
+		c.Brightness = brightnessDefault
+	} else if c.Brightness > 254 {
+		c.Brightness = 254
+	} else if c.Brightness < 1 {
+		c.Brightness = 1
+	}
+
+	c.loaded = true
+
+	return
+}
+
+func (c *ConfigData) Save() (err error) {
+	if !c.loaded {
+		err = &errortypes.WriteError{
+			errors.New("config: Config file has not been loaded"),
+		}
+		return
+	}
+
+	data, err := json.Marshal(c)
+	if err != nil {
+		err = &errortypes.WriteError{
+			errors.Wrap(err, "config: File marshal error"),
+		}
+		return
+	}
+
+	err = ioutil.WriteFile(c.path, data, 0600)
+	if err != nil {
+		err = &errortypes.WriteError{
+			errors.Wrap(err, "config: File write error"),
+		}
+		return
+	}
+
+	return
 }
 
 func (c *ConfigData) CommitAccount(acct *account.Account) (err error) {
@@ -70,71 +171,17 @@ func (c *ConfigData) RemoveAccount(acctId string) (ok bool, err error) {
 }
 
 func Load() (err error) {
-	_, err = os.Stat(filename)
+	err = Config.Load(filename)
 	if err != nil {
-		if os.IsNotExist(err) {
-			err = nil
-		} else {
-			err = &errortypes.ReadError{
-				errors.Wrap(err, "config: File stat error"),
-			}
-		}
 		return
-	}
-
-	file, err := ioutil.ReadFile(filename)
-	if err != nil {
-		err = &errortypes.ReadError{
-			errors.Wrap(err, "config: File read error"),
-		}
-		return
-	}
-
-	err = json.Unmarshal(file, Config)
-	if err != nil {
-		err = &errortypes.ReadError{
-			errors.Wrap(err, "config: File unmarshal error"),
-		}
-		return
-	}
-
-	if Config.Lights == nil {
-		Config.Lights = []string{}
-	}
-
-	if Config.LogPath == "" {
-		Config.LogPath = logPathDefault
-	}
-
-	if Config.ServerPort == 0 {
-		Config.ServerPort = serverPortDefault
-	}
-
-	if Config.ServerHost == "" {
-		Config.ServerHost = serverHostDefault
-	}
-
-	if Config.Accounts == nil {
-		Config.Accounts = map[string]*account.Account{}
 	}
 
 	return
 }
 
 func Save() (err error) {
-	data, err := json.Marshal(Config)
+	err = Config.Save()
 	if err != nil {
-		err = &errortypes.WriteError{
-			errors.Wrap(err, "config: File marshal error"),
-		}
-		return
-	}
-
-	err = ioutil.WriteFile(filename, data, 0644)
-	if err != nil {
-		err = &errortypes.WriteError{
-			errors.Wrap(err, "config: File write error"),
-		}
 		return
 	}
 
